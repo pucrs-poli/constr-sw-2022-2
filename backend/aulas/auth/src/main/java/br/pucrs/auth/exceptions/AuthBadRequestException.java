e4158af6de4298960e17fc8f1ffebddc5efc8fcc
eduardo-arruda <13435770+eduardo-arruda@users.noreply.github.com>
2022-11-21 20:06:54 -0300

    remove submodules

--- backend/aulas/auth/src/main/java/br/pucrs/auth/exceptions/AuthBadRequestException.java
+++
@@ -1,9 +0,0 @@
-package br.pucrs.auth.exceptions;
-
-public class AuthBadRequestException extends RuntimeException {
-    private static final long serialVersionUID = 1L;
-
-    public AuthBadRequestException(String message) {
-        super(message);
-    }
-}
